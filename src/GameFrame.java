d1260ed28f6ebecd62c964d692bf90f2c2653cba
kobayasi towa <zeal24360077@fsgcl.jp>
2025-07-01 10:17:00 +0900

    10章完成

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,47 @@
+import java.util.Vector;
 
-public class GameFrame {
+public class GameFrame extends MyFrame{
+	public void run() {
+		GameWorld.Player=new Player(100,300,0,0);
+		addKeyListener(GameWorld.Player);
+		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		while(true) {
+			clear();
+		GameWorld.Player.draw(this);
+		GameWorld.Player.move();
+		movePlayerBullets();
+		int i=0;
+		while (i<GameWorld.playerBullets.size()) {
+			PlayerBullet b=GameWorld.playerBullets.get(i);
+			b.draw(this);
+			b.move();
+			if(b.y<0) {
+				GameWorld.playerBullets.remove(i);
+			}else {
+				i++;
+			}
+			
+		}
+		sleep(0.03);
+		}
+	}
 
+
+private void movePlayerBullets() {
+	int i=0;
+	while (i<GameWorld.playerBullets.size()) {
+		PlayerBullet b=GameWorld.playerBullets.get(i);
+		b.draw(this);
+		b.move();
+		if (b.y<0) {
+			GameWorld.playerBullets.remove(i);
+		}else {
+			i++;
+		}
+		
+	}
+	// TODO 自動生成されたメソッド・スタブ
+	
 }
+}
+
--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,4 +1,6 @@
+import java.util.Vector;
 
 public class GameWorld {
-
+	 static Player Player;
+	 static Vector<PlayerBullet>playerBullets;
 }
